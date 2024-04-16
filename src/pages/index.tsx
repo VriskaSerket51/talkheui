32f59fd4163676bfe466fb98262739f7e4b041e5
vriskaserket51 <serketvriska51@gmail.com>
2024-04-16 18:29:03 +0900

    chore: code refactor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,33 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 type Operation = Oper | Nothing;
+
 class Oper {
   inner: string;
+
   constructor(inner: string) {
     if (inner.length == 1) {
       this.inner = inner;
-    }
-    else {
+    } else {
       throw new Error("length of inner should be 1");
     }
   }
+
   into(): string {
     return this.inner;
   }
-  is_some(): this is Oper{
+
+  is_some(): this is Oper {
     return true;
   }
 }
+
 class Nothing {
   into(): string {
     return ".";
   }
-  is_some(): this is Oper{
+
+  is_some(): this is Oper {
     return false;
   }
 }
@@ -33,15 +38,20 @@ class Code {
 
   code: Operation[][];
 
-  constructor(row_cnt: number, col_cnt: number, code: Operation[][] | undefined = undefined) {
+  constructor(
+    row_cnt: number,
+    col_cnt: number,
+    code: Operation[][] | undefined = undefined
+  ) {
     this.row_cnt = row_cnt;
     this.col_cnt = col_cnt;
     if (code === undefined) {
       this.code = [...Array(row_cnt)].map<Operation[]>((): Operation[] => {
-        return [...Array(row_cnt)].map<Operation>((): Operation => new Nothing)
+        return [...Array(row_cnt)].map<Operation>(
+          (): Operation => new Nothing()
+        );
       });
-    }
-    else {
+    } else {
       this.code = code;
     }
   }
@@ -50,21 +60,30 @@ class Code {
     const row_idx = cursor.row_idx;
     const col_idx = cursor.col_idx;
 
-    if (row_idx >= 0 && row_idx < this.row_cnt && col_idx >= 0 && col_idx < this.col_cnt) {
+    if (
+      row_idx >= 0 &&
+      row_idx < this.row_cnt &&
+      col_idx >= 0 &&
+      col_idx < this.col_cnt
+    ) {
       return this.code[row_idx][col_idx];
-    }
-    else {
+    } else {
       throw new Error("index out of range");
     }
   }
+
   set(cursor: Cursor, value: Operation) {
     const row_idx = cursor.row_idx;
     const col_idx = cursor.col_idx;
 
-    if (row_idx >= 0 && row_idx < this.row_cnt && col_idx >= 0 && col_idx < this.col_cnt) {
+    if (
+      row_idx >= 0 &&
+      row_idx < this.row_cnt &&
+      col_idx >= 0 &&
+      col_idx < this.col_cnt
+    ) {
       this.code[row_idx][col_idx] = value;
-    }
-    else {
+    } else {
       throw new Error("index out of range");
     }
   }
@@ -77,8 +96,20 @@ class Cursor {
   row_idx: number;
   col_idx: number;
 
-  constructor(row_cnt: number, col_cnt: number, row_idx: number, col_idx: number) {
-    if (row_cnt < 0 || col_cnt < 0 || row_idx < 0 || col_idx < 0 || row_idx >= row_cnt || col_idx >= col_cnt) {
+  constructor(
+    row_cnt: number,
+    col_cnt: number,
+    row_idx: number,
+    col_idx: number
+  ) {
+    if (
+      row_cnt < 0 ||
+      col_cnt < 0 ||
+      row_idx < 0 ||
+      col_idx < 0 ||
+      row_idx >= row_cnt ||
+      col_idx >= col_cnt
+    ) {
       throw new Error("index out of range");
     }
     this.row_cnt = row_cnt;
@@ -118,104 +149,133 @@ class Cursor {
 }
 
 export default function Mainscreen() {
+  const [code, set_code] = useState(new Code(10, 10));
+  const [cursor, set_cursor] = useState(new Cursor(10, 10, 0, 0));
 
-  function AheuiProgram() {
-    const [code, set_code] = useState(new Code(10, 10));
-    const [cursor, set_cursor] = useState(new Cursor(10, 10, 0, 0));
+  const [current_input_idx, set_current_input_idx] = useState(0);
 
-    const [current_input_idx, set_current_input_idx] = useState(0);
+  function keydownHandler(event: React.KeyboardEvent<HTMLInputElement>) {
+    setTimeout(() => {
+      if (event.key === "Backspace") {
+        const new_code = new Code(code.row_cnt, code.col_cnt, code.code);
+        if (!code.get(cursor).is_some()) {
+          set_cursor(cursor.prev());
 
-    function keydownHandler(event: React.KeyboardEvent<HTMLInputElement>) {
-      setTimeout(() => {
-        if (event.key === "Backspace") {
-          const new_code = new Code(code.row_cnt, code.col_cnt, code.code);
-          if (!code.get(cursor).is_some()) {
-            set_cursor(cursor.prev());
-            
-            set_current_input_idx(0);
-            (event.target as HTMLInputElement).value = "";
-          }
-
-          new_code.set(cursor, new Nothing);
-          set_code(new_code);
-
+          set_current_input_idx(0);
+          (event.target as HTMLInputElement).value = "";
         }
-        else {
-          let new_oper: string = (event.target as HTMLInputElement).value;
-          new_oper = new_oper.slice(current_input_idx);
 
-          const new_code = new Code(code.row_cnt, code.col_cnt, code.code);
-          let next_cursor = cursor;
-          let new_current_input_idx = current_input_idx;
+        new_code.set(cursor, new Nothing());
+        set_code(new_code);
+      } else {
+        let new_oper: string = (event.target as HTMLInputElement).value;
+        new_oper = new_oper.slice(current_input_idx);
 
-          while (new_oper.length > 1) {
-            console.log(`length>1 : ${new_oper}`);
-            next_cursor = next_cursor.next();
-            new_code.set(next_cursor, new Oper(new_oper[0]));
+        const new_code = new Code(code.row_cnt, code.col_cnt, code.code);
+        let next_cursor = cursor;
+        let new_current_input_idx = current_input_idx;
 
-            new_oper = new_oper.slice(1);
-            new_current_input_idx += 1;
-          }
+        while (new_oper.length > 1) {
+          console.log(`length>1 : ${new_oper}`);
+          next_cursor = next_cursor.next();
+          new_code.set(next_cursor, new Oper(new_oper[0]));
 
-          if (new_oper.length === 1) {
-            console.log(new_oper);
-            new_code.set(next_cursor, new Oper(new_oper));
-          }
-          else if (new_oper.length === 0) {
-            new_code.set(next_cursor, new Nothing);
-          }
-
-          set_code(new_code);
-          set_cursor(next_cursor);
-          set_current_input_idx(new_current_input_idx);
+          new_oper = new_oper.slice(1);
+          new_current_input_idx += 1;
         }
+
+        if (new_oper.length === 1) {
+          console.log(new_oper);
+          new_code.set(next_cursor, new Oper(new_oper));
+        } else if (new_oper.length === 0) {
+          new_code.set(next_cursor, new Nothing());
+        }
+
+        set_code(new_code);
+        set_cursor(next_cursor);
+        set_current_input_idx(new_current_input_idx);
       }
-        , 0);
-
-    }
-
-    return <div className="aheui-program">
-      {[...Array(code.row_cnt)].map((_, i) =>
-        <Row key={i} code={code} cursor={cursor} row_idx={i} />
-      )}
-      <input type="text" className="input"
-      autoFocus
-        onKeyDown={keydownHandler}></input>
-    </div>
+    }, 0);
   }
 
-  function Row(data: { code: Code, cursor: Cursor, row_idx: number }) {
-    return <div className="row">
-      {[...Array(data.code.col_cnt)].map((_, i) =>
-        <Cell key={i} code={data.code} cursor={data.cursor} row_idx={data.row_idx} col_idx={i} />
-      )}
-    </div>
+  function Row(data: { code: Code; cursor: Cursor; row_idx: number }) {
+    return (
+      <div className="row">
+        {[...Array(data.code.col_cnt)].map((_, i) => (
+          <Cell
+            key={i}
+            code={data.code}
+            cursor={data.cursor}
+            row_idx={data.row_idx}
+            col_idx={i}
+          />
+        ))}
+      </div>
+    );
   }
 
-  function Cell(data: { code: Code, cursor: Cursor, row_idx: number, col_idx: number }) {
-    function cellClickHandler(){
+  function Cell(data: {
+    code: Code;
+    cursor: Cursor;
+    row_idx: number;
+    col_idx: number;
+  }) {
+    function cellClickHandler() {
       // TODO : select the desired input
       document.querySelector("input")!.focus();
 
       // do we have to pass setCursor? idk gg
     }
-    
-    const str = data.code.get(
-      new Cursor(data.code.row_cnt, data.code.col_cnt, data.row_idx, data.col_idx)
-    ).into();
-    if (data.row_idx === data.cursor.row_idx && data.col_idx === data.cursor.col_idx) {
-      return <span className="cell cell-cursor" onClick={cellClickHandler}>{str}</span>
 
-    }
-    else {
-      return <span className="cell" onClick={cellClickHandler}>{str}</span>
+    const str = data.code
+      .get(
+        new Cursor(
+          data.code.row_cnt,
+          data.code.col_cnt,
+          data.row_idx,
+          data.col_idx
+        )
+      )
+      .into();
+    if (
+      data.row_idx === data.cursor.row_idx &&
+      data.col_idx === data.cursor.col_idx
+    ) {
+      return (
+        <span className="cell cell-cursor" onClick={cellClickHandler}>
+          {str}
+        </span>
+      );
+    } else {
+      return (
+        <span className="cell" onClick={cellClickHandler}>
+          {str}
+        </span>
+      );
     }
   }
 
-  return <>
-    <h1>톡희</h1>
-    <div>ㅎㅇ</div>
-    <AheuiProgram></AheuiProgram>
-  </>
+  return (
+    <>
+      <h1>톡희</h1>
+      <button
+        onClick={() => {
+          alert("nah");
+        }}
+      >
+        RUN
+      </button>
+      <div className="aheui-program">
+        {[...Array(code.row_cnt)].map((_, i) => (
+          <Row key={i} code={code} cursor={cursor} row_idx={i} />
+        ))}
+        <input
+          type="text"
+          className="input"
+          autoFocus
+          onKeyDown={keydownHandler}
+        ></input>
+      </div>
+    </>
+  );
 }
-
